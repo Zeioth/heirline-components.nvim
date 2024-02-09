f9fab7be24ebee45c3088a24be06cf293fa313ec
Zeioth <zeioth@hotmail.com>
2024-02-10 00:49:24 +0100

    Initial commit.

---
+++ lua/heirline-components/all.lua
@@ -0,0 +1,16 @@
+--- ### Heirline components main file.
+--
+--  DESCRIPTION:
+--  We use this file to configure the plugin "Heirline".
+--  By having this file, we don't have to require every file separately.
+
+return {
+  component = require "heirline-components.core.component",
+  condition = require "heirline-components.core.condition",
+  env = require "heirline-components.core.env",
+  heirline = require "heirline-components.core.heirline",
+  hl = require "heirline-components.core.hl",
+  init = require "heirline-components.core.init",
+  provider = require "heirline-components.core.provider",
+  utils = require "heirline-components.core.utils",
+}
