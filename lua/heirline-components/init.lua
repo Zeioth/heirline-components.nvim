091e4ee8f244425366bafbecfbfe146ee6f56229
Zeioth <zeioth@hotmail.com>
2024-02-14 19:11:30 +0100

    revert: Previous commit.

--- lua/heirline-components/init.lua
+++ lua/heirline-components/init.lua
@@ -9,8 +9,6 @@ local config = require "heirline-components.config"
 
 M.setup = function(opts)
   config.set(opts)
-  require("heirline-components.all").init.subscribe_to_events()
-
 end
 
 return M
