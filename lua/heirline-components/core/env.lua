e738a7d7873034f0842c9018a9dafeaa71f72fdc
Zeioth <zeioth@hotmail.com>
2024-02-14 18:33:26 +0100

    ✨feat(opts): New option `colors` added.

--- lua/heirline-components/core/env.lua
+++ lua/heirline-components/core/env.lua
@@ -1,29 +1,11 @@
 --- ### Heirline environment.
 --
 -- DESCRIPTION:
--- Constants we use mostly on heirline.
+-- Constants we use on our components.
 
 local M = {}
 
-M.fallback_colors = {
-  none = "NONE",
-  fg = "#abb2bf",
-  bg = "#1e222a",
-  dark_bg = "#2c323c",
-  blue = "#61afef",
-  green = "#98c379",
-  grey = "#5c6370",
-  bright_grey = "#777d86",
-  dark_grey = "#5c5c5c",
-  orange = "#ff9640",
-  purple = "#c678dd",
-  bright_purple = "#a9a1e1",
-  red = "#e06c75",
-  bright_red = "#ec5f67",
-  white = "#c9c9c9",
-  yellow = "#e5c07b",
-  bright_yellow = "#ebae34",
-}
+M.fallback_colors = vim.g.heirline_components_config.colors
 
 M.modes = {
   ["n"] = { "NORMAL", "normal" },
