fc8e59a9c3ea05b2aca3095f0cf42fd1211d3842
Zeioth <zeioth@hotmail.com>
2024-05-25 02:33:46 +0200

    refactor: The function `M.get_hlgroup` don't require to check for termguicolors anymore.

--- lua/heirline-components/core/hl.lua
+++ lua/heirline-components/core/hl.lua
@@ -82,18 +82,11 @@ end
 ---@return table properties # the highlight group properties.
 function M.get_hlgroup(name, fallback)
   if vim.fn.hlexists(name) == 1 then
-    local hl
-    if vim.api.nvim_get_hl then -- check for new neovim 0.9 API
-      hl = vim.api.nvim_get_hl(0, { name = name, link = false })
-      if not hl.fg then hl.fg = "NONE" end
-      if not hl.bg then hl.bg = "NONE" end
-    else
-      hl = vim.api.nvim_get_hl_by_name(name, vim.o.termguicolors)
-      if not hl.foreground then hl.foreground = "NONE" end
-      if not hl.background then hl.background = "NONE" end
-      hl.fg, hl.bg = hl.foreground, hl.background
-      hl.ctermfg, hl.ctermbg = hl.fg, hl.bg
-      hl.sp = hl.special
+    local hl = vim.api.nvim_get_hl(0, { name = name, link = false })
+    if not hl.fg then hl.fg = "NONE" end
+    if not hl.bg then hl.bg = "NONE" end
+    if hl.reverse then
+      hl.fg, hl.bg, hl.reverse = hl.bg, hl.fg, nil
     end
     return hl
   end
